6f963a53732ed2fe39306fe31b4a9cf7df7b4ab9
imraghav20 <raghav20nov@gmail.com>
2020-09-02 00:01:57 +0530

    restructured the code

---
+++ stopstalk_app/lib/classes/user_editorial_class.dart
@@ -0,0 +1,9 @@
+class UserEditorial {
+  final String editorial_count;
+  final String votes_count;
+  final String name;
+  final String username;
+
+  UserEditorial(
+      this.editorial_count, this.votes_count, this.username, this.name);
+}
---
+++ stopstalk_app/lib/fragments/editorials.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Editorials extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Editorials Page'),
+    );
+  }
+
+}
