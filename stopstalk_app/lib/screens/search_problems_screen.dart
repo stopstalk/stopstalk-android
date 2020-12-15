d5b98500d412c943bcfaedfbf4e41b5b7e7b7dd0
imraghav20 <raghav20nov@gmail.com>
2020-12-15 20:48:53 +0530

    modified hint color

--- stopstalk_app/lib/screens/search_problems_screen.dart
+++ stopstalk_app/lib/screens/search_problems_screen.dart
@@ -130,7 +130,7 @@ class _SearchProblemsScreenState extends State<SearchProblemsScreen> {
                       hintStyle: TextStyle(
                           fontSize: 16.0,
                           fontWeight: FontWeight.w400,
-                          color: Colors.blueGrey),
+                          color: Colors.black54),
                     ),
                   ),
                   Padding(
@@ -203,7 +203,7 @@ class _SearchProblemsScreenState extends State<SearchProblemsScreen> {
                       hintStyle: TextStyle(
                           fontSize: 16.0,
                           fontWeight: FontWeight.w400,
-                          color: Colors.blueGrey),
+                          color: Colors.black54),
                     ),
                   ),
                   Padding(
