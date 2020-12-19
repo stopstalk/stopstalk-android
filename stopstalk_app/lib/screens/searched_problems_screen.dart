d400a9736bca5b04d653e297d4321668bf35322f
imraghav20 <raghav20nov@gmail.com>
2020-12-20 00:40:59 +0530

    modified No Results Found Image position

--- stopstalk_app/lib/screens/searched_problems_screen.dart
+++ stopstalk_app/lib/screens/searched_problems_screen.dart
@@ -161,16 +161,9 @@ class _SearchedProblemsScreenState extends State<SearchedProblemsScreen> {
     return AnimatedOpacity(
       opacity: 1,
       duration: Duration(milliseconds: 600),
-      child: Container(
-        height: MediaQuery.of(context).size.height * 0.3,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image(
-              image: AssetImage('assets/images/noResult.png'),
-            )
-          ],
+      child: Center(
+        child: Image(
+          image: AssetImage('assets/images/noResult.png'),
         ),
       ),
     );
